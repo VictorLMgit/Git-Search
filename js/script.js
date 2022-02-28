f1221ca02f070f7859662e567a21a19236b10f3e
VictorLMgit <victorlimacar@gmail.com>
2022-02-28 19:31:54 -0300

    create repositories

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+// Search
 form = document.getElementById('mainForm');
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
+    document.getElementById('repos').innerHTML = ''
     var search = document.getElementById('search').value;
     userName = search.replace(' ', '')
     
@@ -11,6 +13,28 @@ form.addEventListener('submit',function(e){
         
         document.getElementById('photo').innerHTML = "<a target = '_blank' href ='https://github.com/"+data['login']+"'>  <img class='img-fluid w-50 rounded-circle ' src="+data['avatar_url']+" </img> </a>"
         document.getElementById('info').innerHTML = "<p class='text-center text-light'>"+data['login']+"</p><p class='text-center text-light'>"+data['bio']+"</p>"
-    })
+        var repos = data['repos_url'];
+        fetch(repos).then((result) => result.json())
+        .then((data_repos)=>{
+            var tag = document.createElement("h3");
+            var text = document.createTextNode('Repositórios');
+            tag.appendChild(text);
+            document.getElementById("repos").appendChild(tag);
+       
+            data_repos.forEach((data_repos) => {
 
+                var tag = document.createElement("p");
+                var text = document.createTextNode(data_repos['name']);
+                tag.appendChild(text);
+                var element = document.getElementById("repos");
+                element.appendChild(tag);
+
+                // console.log(data_repos['name'])
+            });
+            
+            
+        })
+        
+    })
 })
+
